d6a75142f88319cd6c67d0e8888569637a914207
KevinQqs <ksquinones09@ucatolica.edu.co>
2026-05-08 07:28:18 -0500

    primer commit

--- model.py
+++ model.py
@@ -1,20 +1,16 @@
 from sqlmodel import Field, SQLModel
-
 from datetime import datetime, timezone
 
 
 class Dog(SQLModel, table = True):
     __tablename__ = "dogs"
-
-    created: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
-
-class Sticker(SQLModel, table = True):
-    __tablename__ = "stickers"
-
-    created: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
-
-
-class Book(SQLModel, table = True):
-    __tablename__ = "books"
+    id: int
+    name: str
+    size: str
+    dangerous: bool
+    sterilized: bool
+    breed: str
+    created: datetime
+    img: str
 
     created: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
